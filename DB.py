ef1daf73701cd36971812c85b3f36270c2adb30f
Farshad Bolouri <bolouri.farshad@gmail.com>
2020-01-10 20:56:23 +0330

    some of functions and built in modules used for sqlite3  in python

--- DB.py
+++ DB.py
@@ -5,7 +5,14 @@ import sqlite3
 Create a new sqlite database 
 """
 database = sqlite3.connect('our_data.db')
+print("Table created")
 print("Database opened.")
+"""
+We use Cursor because it's so faster
+"""
+cursor = database.cursor()
+print("Cursor created")
+
 
 """ 
 'CREATE TABLE' command will create a table in database using SQL language
@@ -13,7 +20,7 @@ print("Database opened.")
 """
 
 
-database.execute(""" CREATE TABLE IF NOT EXISTS employee_records(
+cursor.execute(""" CREATE TABLE IF NOT EXISTS employee_records(
     ID INT PRIMARY KEY NOT NULL,
     NAME TEXT NOT NULL,
     DIVISION TEXT NOT NULL,
@@ -22,7 +29,7 @@ database.execute(""" CREATE TABLE IF NOT EXISTS employee_records(
 
 
 def insert_record(id, name, division, stars):
-    database.execute(""" INSERT INTO employee_records(ID,NAME,DIVISION,STARS)
+    cursor.execute(""" INSERT INTO employee_records(ID,NAME,DIVISION,STARS)
         VALUES(?,?,?,?)""", (id, name, division, stars))
     database.commit()
     print("Record inserted")
@@ -37,20 +44,40 @@ SELECT *  FROM employee_records
 
 
 def read_data():
-    data = database.execute(""" SELECT * FROM employee_records
-""")
+    data = cursor.execute(""" SELECT * FROM employee_records ORDER BY NAME""")
+    x = cursor.fetchall()
     for record in data:
         print('ID : ', record[0])
         print('NAME : ', record[1])
         print('DIVISION : ', record[2])
         print('STARTS : ', record[3])
         print()
+    print("==================")
+    print(x)
+
+"""
+We MUST use build in modules after inserting and updating in order to
+import them into database
+"""
+
+
+def update_record():
+    cursor.execute(""" UPDATE employee_records SET DIVISION = 'Engineer'  WHERE ID = 4 """)
+    database.commit()
+    print("Record updated")
+
+
+def delete_record():
+    cursor.execute(""" DELETE FROM  employee_records WHERE ID = 3 """)
+    database.commit()
+    print("Record deleted")
+
 
 # insert_record(5, "John", "Lawyer", 3)
 
-
+delete_record()
+update_record()
 read_data()
-print("Table created")
 database.close()
 print("Database closed.")
 
